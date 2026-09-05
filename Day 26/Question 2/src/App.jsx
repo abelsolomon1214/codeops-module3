52acc181b1fede569dd3fa561a985d6837c5ed9f
abelsolomon1214 <solomonabel369@gmail.com>
2026-09-05 14:44:10 +0300

    Day 26: Question 2 custom JSX

---
+++ Day 26/Question 2/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main>
+      <h1>Welcome to Addis Eats</h1>
+      <p>Discover delicious Ethiopian food made with tradition and flavor.</p>
+    </main>
+  );
+}
+
+export default App;
